3b37dd828bcf257916efe302dd67435e6686dd2b
priti-jaiswar <jaiswarpriti58@gmail.com>
2024-10-29 18:59:04 +0530

    nothing

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,19 @@
 
+"use client";
+ 
+import { motion } from "framer-motion";
+import React from "react";
+import { AuroraBackground } from "./components/ui/aurora-background";
 import URLShortenerInput from "./components/URLShortner";
 
 export default function Home() {
   return (
-   <URLShortenerInput/>
+
+    <AuroraBackground>
+     
+
+<URLShortenerInput />
+      
+    </AuroraBackground>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,31 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      animation: {
+        aurora: "aurora 60s linear infinite",
+      },
+      keyframes: {
+        aurora: {
+          from: {
+            backgroundPosition: "50% 50%, 50% 50%",
+          },
+          to: {
+            backgroundPosition: "350% 50%, 350% 50%",
+          },
+        },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
+}
 };
+
+function addVariablesForColors( addBase, theme ) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
